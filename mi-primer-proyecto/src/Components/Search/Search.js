ca3157f18b80a7544bdb025e41073cf683949e16
Desmondrattagan <drattagan@udesa.edu.ar>
2024-04-14 15:13:47 -0300

    Search

--- mi-primer-proyecto/src/Components/Search/Search.js
+++ mi-primer-proyecto/src/Components/Search/Search.js
@@ -2,18 +2,31 @@ import { Component } from 'react'
 
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {valor: ''};
+    }
+
+    evitarSubmit(event){
+        event.preventDefault();
+    }
+
+    controlarCambios(event){
+        this.setState({valor: event.target.value},
+            ()=>console.log(event))
+    }
+    
     render() {
         return (
-            <div className="searchForm">
-                
-                <form>
-                    <input type="buscar" className="buscador" name="busqueda" placeholder="Search..." />
-                    <button className="buscador" type="enviar"><i
-                        className="fa-solid fa-magnifying-glass fa-beat-fade"></i></button>
-                </form>
-            </div>
-        )
+           <form onSubmit={ (event)=> this.evitarSubmit(event)}>
+            <label>Search Movies</label>
+            <input type='text' onChange={(event)=> this.controlarCambios(event)} />
+            <input type= 'submit' value='Submit'/>
+            </form>
+        );
     }
 }
+
+//{<SearchResults value= {this.state.valor}/>}
 
 export default Search
---
+++ mi-primer-proyecto/src/screens/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchResults from "../Search/Search"
+
+function SearchResults(props){
+
+    return (
+        <div>
+       
+      
+        </div>
+    )
+}
+
+export default SearchResults

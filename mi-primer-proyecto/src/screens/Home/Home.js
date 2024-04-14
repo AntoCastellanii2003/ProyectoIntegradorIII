165cf52c3d84220a9b7c936c8479b357e064f3fa
AntoCastellanii2003 <acastellani@udesa.edu.ar>
2024-04-14 14:42:51 -0300

    Home

---
+++ mi-primer-proyecto/src/screens/Home/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import TopRated from "../../Components/TopRated/TopRated";
+import NowPlaying from "../../Components/NowPlaying/NowPlaying";
+import home from "./home.css";
+
+class Home extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      };
+    }
+
+
+    render() {
+      return (
+        <main>
+  
+          <section>
+            <h1 className="TitleHome">TOP RATED</h1>
+            <TopRated />
+            <h1 className="TitleHome">NOW PLAYING</h1>
+            <NowPlaying />
+           
+          </section>
+        </main>
+      
+      );
+    }
+   
+  
+  }
+  
+  export default Home;
+

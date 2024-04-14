86f72cbf61fc9ab1b3fa98a1fb8974d61f31b57f
florenciapreuss <fpreuss@udesa.edu.ar>
2024-04-14 14:27:36 -0300

    empece con la navegacion

--- mi-primer-proyecto/src/index.js
+++ mi-primer-proyecto/src/index.js
@@ -1,7 +1,11 @@
-
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+);

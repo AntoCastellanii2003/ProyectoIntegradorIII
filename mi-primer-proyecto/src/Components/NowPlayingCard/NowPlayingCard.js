093894c7ae8c2d03f08752316be7f3f88ef241c3
florenciapreuss <fpreuss@udesa.edu.ar>
2024-04-15 17:03:57 -0300

    hice movie details

--- mi-primer-proyecto/src/Components/NowPlayingCard/NowPlayingCard.js
+++ mi-primer-proyecto/src/Components/NowPlayingCard/NowPlayingCard.js
@@ -14,10 +14,11 @@ class NowPlayingCard extends Component{
      { 
         console.log(this.props.datos) 
     return(<article className= "NowPlayingCard">
+        <Link to={"/MovieDetails/id/" + this.props.datos.id}>
             <img src={this.props.datos.poster_path}/>
             <h2>{this.props.datos.title}</h2>
             <img className="imgcard"src={`https://image.tmdb.org/t/p/w500/${this.props.datos.poster_path}/images`} alt={this.props.datos.title}></img>
-            
+            </Link>
        </article>)}
 }
 

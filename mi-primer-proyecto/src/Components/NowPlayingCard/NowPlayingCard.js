344426f711ea3c6772e3208f90051a55882cab61
florenciapreuss <fpreuss@udesa.edu.ar>
2024-04-18 23:57:10 -0300

    agregue boton de addtofavorites en home

--- mi-primer-proyecto/src/Components/NowPlayingCard/NowPlayingCard.js
+++ mi-primer-proyecto/src/Components/NowPlayingCard/NowPlayingCard.js
@@ -1,31 +1,43 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-
-class NowPlayingCard extends Component{
-
-    constructor(props){
-        super(props)
+class NowPlayingCard extends Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            
-        }
+            isFavorite: localStorage.getItem('favorites') && localStorage.getItem('favorites').includes(this.props.datos.id.toString())
+        };
     }
 
-    render()
-     { 
-        console.log(this.props.datos) 
-    return(<article className= "NowPlayingCard">
-        <Link to={"/MovieDetails/id/" + this.props.datos.id}>
-            <img src={this.props.datos.poster_path}/>
-            <h2>{this.props.datos.title}</h2>
-            <img className="imgcard"src={`https://image.tmdb.org/t/p/w500/${this.props.datos.poster_path}/images`} alt={this.props.datos.title}></img>
-            </Link>
-       </article>)}
+    addToFavorites = () => {
+        let favorites = localStorage.getItem('favorites') ? JSON.parse(localStorage.getItem('favorites')) : [];
+        favorites.push(this.props.datos.id.toString());
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+        this.setState({ isFavorite: true });
+    };
+
+    removeFromFavorites = () => {
+        let favorites = JSON.parse(localStorage.getItem('favorites'));
+        let filteredFavorites = favorites.filter(id => id !== this.props.datos.id.toString());
+        localStorage.setItem('favorites', JSON.stringify(filteredFavorites));
+        this.setState({ isFavorite: false });
+    };
+
+    render() {
+        return (
+            <article className="NowPlayingCard">
+                <Link to={`/MovieDetails/id/${this.props.datos.id}`}>
+                    <img src={`https://image.tmdb.org/t/p/w500/${this.props.datos.poster_path}`} alt={this.props.datos.title} />
+                    <h2>{this.props.datos.title}</h2>
+                </Link>
+                {this.state.isFavorite ? (
+                    <button onClick={this.removeFromFavorites}>Remove from Favorites</button>
+                ) : (
+                    <button onClick={this.addToFavorites}>Add to Favorites</button>
+                )}
+            </article>
+        );
+    }
 }
 
-{/* <img src={this.props.datos.poster_path}/>
-            <h2>{this.props.data.title}</h2>
-            <img className="imgcard"src={`https://image.tmdb.org/t/p/w500/${this.props.datos.poster_path}/images`} alt={this.props.datos.title}></img>
-             */}
-
-        
 export default NowPlayingCard;
